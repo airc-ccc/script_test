dfa8130b12e12574058f764dd19fb1c8413362d0
pengtao <pengtaopt01@163.com>
2021-06-01 23:30:42 +0800

    寻找最长回文字符串;运行超时

--- src/com/justdoit/Main.java
+++ src/com/justdoit/Main.java
@@ -1,15 +1,14 @@
 package com.justdoit;
 
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         int x = 16;
+		// String s = "whdqcudjpisufnrtsyupwtnnbsvfptrcgvobbjglmpynebblpigaflpbezjvjgbmofejyjssdhbgghgrhzuplbeptpaecfdanhlylgusptlgobkqnulxvnwuzwauewcplnvcwowmbxxnhsdmgxtvbfgnuqdpxennqglgmspbagvmjcmzmbsuacxlqfxjggrwsnbblnnwisvmpwwhomyjylbtedzrptejjsaiqzprnadkjxeqfdpkddmbzokkegtypxaafodjdwirynzurzkjzrkufsokhcdkajwmqvhcbzcnysrbsfxhfvtodqabvbuosxtonbpmgoemcgkudandrioncjigbyizekiakmrfjvezuzddjxqyevyenuebfwugqelxwpirsoyixowcmtgosuggrkdciehktojageynqkazsqxraimeopcsjxcdtzhlbvtlvzytgblwkmbfwmggrkpioeofkrmfdgfwknrbaimhefpzckrzwdvddhdqujffwvtvfyjlimkljrsnnhudyejcrtrwvtsbkxaplchgbikscfcbhovlepdojmqybzhbiionyjxqsmquehkhzdiawfxunguhqhkxqdiiwsbuhosebxrpcstpklukjcsnnzpbylzaoyrmyjatuovmaqiwfdfwyhugbeehdzeozdrvcvghekusiahfxhlzclhbegdnvkzeoafodnqbtanfwixjzirnoaiqamjgkcapeopbzbgtxsjhqurbpbuduqjziznblrhxbydxsmtjdfeepntijqpkuwmqezkhnkwbvwgnkxmkyhlbfuwaslmjzlhocsgtoujabbexvxweigplmlewumcone";
         System.out.println(longestPalindrome());
+        // System.out.println(longestPalindromeHaha(s));
     }
 
 
@@ -90,31 +89,74 @@ public class Main {
 	}
 
 
-	public static String longestPalindrome() {
-	    String s = "aaf";
+
+    public static String longestPalindrome() {
+	    String s = "whdqcudjpisufnrtsyupwtnnbsvfptrcgvobbjglmpynebblpigaflpbezjvjgbmofejyjssdhbgghgrhzuplbeptpaecfdanhlylgusptlgobkqnulxvnwuzwauewcplnvcwowmbxxnhsdmgxtvbfgnuqdpxennqglgmspbagvmjcmzmbsuacxlqfxjggrwsnbblnnwisvmpwwhomyjylbtedzrptejjsaiqzprnadkjxeqfdpkddmbzokkegtypxaafodjdwirynzurzkjzrkufsokhcdkajwmqvhcbzcnysrbsfxhfvtodqabvbuosxtonbpmgoemcgkudandrioncjigbyizekiakmrfjvezuzddjxqyevyenuebfwugqelxwpirsoyixowcmtgosuggrkdciehktojageynqkazsqxraimeopcsjxcdtzhlbvtlvzytgblwkmbfwmggrkpioeofkrmfdgfwknrbaimhefpzckrzwdvddhdqujffwvtvfyjlimkljrsnnhudyejcrtrwvtsbkxaplchgbikscfcbhovlepdojmqybzhbiionyjxqsmquehkhzdiawfxunguhqhkxqdiiwsbuhosebxrpcstpklukjcsnnzpbylzaoyrmyjatuovmaqiwfdfwyhugbeehdzeozdrvcvghekusiahfxhlzclhbegdnvkzeoafodnqbtanfwixjzirnoaiqamjgkcapeopbzbgtxsjhqurbpbuduqjziznblrhxbydxsmtjdfeepntijqpkuwmqezkhnkwbvwgnkxmkyhlbfuwaslmjzlhocsgtoujabbexvxweigplmlewumcone";
 	    int sLen = s.length();
-        StringBuilder longestX = new StringBuilder();
-		StringBuilder longestY = new StringBuilder();
-		int i = 0;
-		while(i < sLen) {
-            for (char c : s.toCharArray()) {
-                longestX.append(String.valueOf(c));
-                String oldLongest = longestX.toString();
-                String newLongest = longestX.reverse().toString();
-                if (newLongest.equals(oldLongest) && longestX.length() > longestY.length()) {
-                    // Is palindrome
-                    longestY = longestX;
-                    longestX = new StringBuilder();
-                } else {
-                    longestX.reverse();
+        if (sLen < 2) {
+            return s;
+        }
+        char[] sCharList = s.toCharArray();
+        StringBuffer longestX = new StringBuffer();
+		StringBuffer longestY = new StringBuffer();
+        for (int i = 0; i < sLen; i++) {
+            char[] sTwoCharList = s.toCharArray();
+            for (char c2 : sTwoCharList) {
+                longestX.append(c2);
+                if (isPalinaStr(longestX) && longestX.length() > longestY.length()) {
+                    String trLongest = longestX.toString();
+                    longestY = new StringBuffer(trLongest);
                 }
             }
-            longestX = new StringBuilder();
-            StringBuilder new_s = new StringBuilder(s);
+            longestX = new StringBuffer();
+            StringBuffer new_s = new StringBuffer(s);
             new_s.delete(0, 1);
             s = new_s.toString();
-            i++;
         }
-        return String.valueOf(longestY);
+        return longestY.toString();
 	}
+    
+
+    public static boolean isPalinaStr(StringBuffer sb) {
+        String oldLongest = sb.toString();
+        StringBuffer newLongestX = new StringBuffer(oldLongest);
+        String newLongest = newLongestX.reverse().toString();
+        return oldLongest.equals(newLongest);
+    }
+
+    public static String longestPalindromeHaha(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+//         保存起始位置，测试了用数组似乎能比全局变量稍快一点
+        int[] range = new int[2];
+        char[] str = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+//             把回文看成中间的部分全是同一字符，左右部分相对称
+//             找到下一个与当前字符不同的字符
+            i = findLongest(str, i, range);
+        }
+        return s.substring(range[0], range[1] + 1);
+    }
+    
+    public static int findLongest(char[] str, int low, int[] range) {
+//         查找中间部分
+        int high = low;
+        while (high < str.length - 1 && str[high + 1] == str[low]) {
+            high++;
+        }
+//         定位中间部分的最后一个字符
+        int ans = high;
+//         从中间向左右扩散
+        while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+            low--;
+            high++;
+        }
+//         记录最大长度
+        if (high - low > range[1] - range[0]) {
+            range[0] = low;
+            range[1] = high;
+        }
+        return ans;
+    }
 }
